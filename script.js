1ba33686085d011b34f37fc38c04551fb56d1f4c
ZuuZoo <88343151+ZuuZoo@users.noreply.github.com>
2025-12-10 09:50:02 +0100

    Updated

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+
+var randomNumber = random8();
+
+
 function loaded() {
+    active = 1
     console.log("Page loaded");
-    var randomNumber = Math.floor(Math.random() * 8) + 1;
     document.getElementById("main_finger").src = "pelda" + randomNumber + ".png";
     console.log("Image set to pelda" + randomNumber + ".png");
+
+    document.getElementById("sor1_image").src = "pelda" +randomNumber +"/"+random8()+".png"
+    document.getElementById("sor2_image").src = "pelda" +randomNumber +"/"+random8()+".png"
+    document.getElementById("sor3_image").src = "pelda" +randomNumber +"/"+random8()+".png"
+    document.getElementById("sor4_image").src = "pelda" +randomNumber +"/"+random8()+".png"
+    document.getElementById("sor5_image").src = "pelda" +randomNumber +"/"+random8()+".png"
+    document.getElementById("sor6_image").src = "pelda" +randomNumber +"/"+random8()+".png"
+    document.getElementById("sor7_image").src = "pelda" +randomNumber +"/"+random8()+".png"
+    document.getElementById("sor8_image").src = "pelda" +randomNumber +"/"+random8()+".png"
 }
+function random8() {
+    return Math.floor(Math.random() * 8) + 1;
+}
+
+
+
+
+/*document.getElementById("sor1_image").classList.remove("active")*/
